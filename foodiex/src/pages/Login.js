9f9f799955726a5a44da79e484ebfb310e2526e3
SalmanAlnajdi <salman.it095@gmail.com>
2024-07-02 19:02:16 +0300

    Login page done

--- foodiex/src/pages/Login.js
+++ foodiex/src/pages/Login.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Svg1 } from "../assets/1.svg";
+import { login } from "../api/auth";
+import { useMutation } from "@tanstack/react-query";
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({});
+
+  const handleChange = (e) => {
+    setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
+  };
+
+  const { mutate } = useMutation({
+    mutationKey: ["login"],
+    mutationFn: () => login(userInfo),
+    onSuccess: () => {
+      setUserInfo(true);
+    },
+  });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    mutate();
+  };
+
   return (
     //Background
     <div className="bg-slate-800">
@@ -43,6 +64,7 @@ const Login = () => {
                     name="username"
                     type="username"
                     autoComplete="username"
+                    onChange={handleChange}
                     required
                     className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -71,6 +93,7 @@ const Login = () => {
                     id="password"
                     name="password"
                     type="password"
+                    onChange={handleChange}
                     autoComplete="current-password"
                     required
                     className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -81,6 +104,7 @@ const Login = () => {
               <div>
                 <button
                   type="submit"
+                  onClick={handleFormSubmit}
                   className="flex w-full justify-center rounded-md bg-orange-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Login
